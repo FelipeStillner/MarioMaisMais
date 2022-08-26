368e52234e5ac8129303cc2c97985b9a48091ef5
FelipeStillner <99509153+FelipeStillner@users.noreply.github.com>
2022-08-26 13:10:40 -0300

    Add files via upload

--- Principal.cpp
+++ Principal.cpp
@@ -10,5 +10,28 @@ Principal::~Principal()
 
 void Principal::exec()
 {
+  sf::RenderWindow window(sf::VideoMode(300, 300), "NOME");
+  
+  sf::Clock clock;
 
+  while (window.isOpen())
+  {
+    sf::Event evnt;
+
+    while (window.pollEvent(evnt))
+    {
+      switch (evnt.type)
+      {
+      case sf::Event::Closed:
+        window.close();
+        break;
+      
+      default:
+        break;
+      }
+    }
+
+    window.clear(sf::Color::Blue);
+    window.display();
+  }
 }
