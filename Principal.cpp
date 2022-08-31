732baaae5fdf137e0842651f54b57fce749e93d9
FelipeStillner <felipeeufranio@gmail.com>
2022-08-31 15:37:54 -0300

    .

--- Principal.cpp
+++ Principal.cpp
@@ -12,21 +12,31 @@ Principal::~Principal()
 
 void Principal::executar()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    // Load a sprite to display
+    sf::Texture texture;
+    if (!texture.loadFromFile("a.png"))
+        return;
+    sf::Sprite sprite(texture, sf::IntRect(0, 75, 21, 40));
+    sprite.move(sf::Vector2f(100, 100));
 
+    // Start the game loop
     while (window.isOpen())
     {
+        // Process events
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // Close window: exit
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        // Clear screen
         window.clear();
-        window.draw(shape);
+        // Draw the sprite
+        window.draw(sprite);
+        // Update the window
         window.display();
     }
 }
