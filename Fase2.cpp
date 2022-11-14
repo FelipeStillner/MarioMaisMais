59270a6864db6afabcd62a22706aa5621e8c44c3
FelipeStillner <felipeeufranio@gmail.com>
2022-11-14 09:21:43 -0300

    .

--- Fase2.cpp
+++ Fase2.cpp
@@ -7,9 +7,9 @@ Fase2::Fase2(Principal* prin) : Fase(prin)
 
   create(PLAYER, 400.0, 200.0);
   create(PLAYER, 500.0, 200.0);
-  create(ENEM1, 1000.0, 300.0);
-  create(ENEM2, 1200.0, 300.0);
-  //create(TUBO, 1000.0, 600.0);
+  create(ENEM1, 1200.0, 300.0);
+  create(ENEM2, 1400.0, 300.0);
+  create(TUBO, 1000.0, 600.0);
   create(CHAO, 0.0, 700.0);
 }
 
